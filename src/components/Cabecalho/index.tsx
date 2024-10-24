e4f17af9de896d565521a9672217f8d3145f0ffb
Thais Silva <141892213+Thais-Silva27@users.noreply.github.com>
2024-10-24 20:26:36 -0300

    Add files via upload

---
+++ src/components/Cabecalho/index.tsx
@@ -0,0 +1,9 @@
+import { CabecalhoSite } from './Cabecalho.module.css'
+
+const Cabecalho = () => (
+  <CabecalhoSite>
+    <h1>EBAC Jobs</h1>
+  </CabecalhoSite>
+)
+
+export default Cabecalho
---
+++ src/components/FormVagas/index.tsx
@@ -0,0 +1,28 @@
+import { FormEvent, useState } from 'react'
+import { Form, BtnPesquisar, Campo } from './FormVagas.module.css'
+
+type Props = {
+  aoPesquisar: (termo: string) => void
+}
+
+const FormVagas = ({ aoPesquisar }: Props) => {
+  const [termo, setTermo] = useState<string>('')
+
+  const aoEnviarForm = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    aoPesquisar(termo.toLocaleLowerCase())
+  }
+
+  return (
+    <Formulario onSubmit={aoEnviarForm}>
+      <Campo
+        placeholder="Front-end, fullstack, node, design"
+        onChange={(e) => setTermo(e.target.value)}
+        type="search"
+      />
+      <BotaoPesquisar type="submit">Pesquisar</BotaoPesquisar>
+    </Formulario>
+  )
+}
+
+export default FormVagas
