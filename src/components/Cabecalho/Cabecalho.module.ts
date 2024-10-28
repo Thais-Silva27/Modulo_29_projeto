24a9e2820b959e49a3d40ca928fc71fa09208583
Thais Silva <141892213+Thais-Silva27@users.noreply.github.com>
2024-10-28 10:56:18 -0300

    Delete src directory

--- src/components/Cabecalho/Cabecalho.module.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-export const CabecalhoSite = styled.header`
-  background-color: var(--cor-secundaria);
-  padding: 24px 0;
-  color: var(--cor-principal);
-  text-align: center;
-`
-
